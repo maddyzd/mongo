8761371e9726c166125863e1870567ef4b43fc1c
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 16:39:57 -0400

    Create mongo.js

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+	  
+console.log('hey')
+  MongoClient.connect(connStr, function(err, db) {
+    
+  if(err) { console.log(err); }
+  else {
+    var dbo = db.db("Stock");
+	  var collection = dbo.collection('PublicCompanies');
+    console.log("Success!");
+	  db.close();
+  }
+});
