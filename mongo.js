5032960d52ec214cfe08afd6082e8139bb6bf5f3
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 16:53:51 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,14 +1,26 @@
+const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-	  
-console.log('hey')
+
+const app = express();
+
+app.get('/', (req, res) => {
   MongoClient.connect(connStr, function(err, db) {
-    
-  if(err) { console.log(err); }
-  else {
-    var dbo = db.db("Stock");
-	  var collection = dbo.collection('PublicCompanies');
-    console.log("Success!");
-	  db.close();
-  }
+    if (err) {
+      console.log(err);
+      res.send('Error connecting to database');
+    } else {
+      var dbo = db.db("Stock");
+      var collection = dbo.collection('PublicCompanies');
+      console.log("Success!");
+      db.close();
+      res.send('Database connection successful!');
+    }
+  });
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
